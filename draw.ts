69caeed27c79eab4ce1286ec12a13c0878b170fd
Eduard Rzhautski <eduard.rzhautski@gmail.com>
2022-06-24 04:20:15 +0300

    refactor: Circle, Square, Rectangular

--- draw.ts
+++ draw.ts
@@ -7,7 +7,6 @@ export const drawCircle = async (data: RawData) => {
         const radius = +data.toString().split(' ')[1];
 
         const mousePos = robot.getMousePos();
-        robot.setMouseDelay(8);
 
         for (let i = 0; i <= Math.PI * 2; i += 0.01) {
 
@@ -15,10 +14,10 @@ export const drawCircle = async (data: RawData) => {
             const y = mousePos.y + (radius * Math.sin(i));
 
             robot.dragMouse(x, y);
-
             robot.mouseToggle('down');
-            robot.mouseToggle("up");
         };
+
+        robot.mouseToggle("up");
 
     } catch (error) {
         console.log(error);
@@ -30,50 +29,16 @@ export const drawSquare = async (data: RawData) => {
     try {
         const width = +data.toString().split(' ')[1];
 
-        robot.setMouseDelay(5);
-
         const side = robot.getMousePos();
+        let x = side.x;
+        let y = side.y;
 
-        for (let i = 0; i < width; i++) {
-            let x = side.x;
-            let y = side.y;
-
-            robot.moveMouseSmooth(x + i, y);
-            robot.mouseToggle('down');
-        }
-        const side2 = robot.getMousePos();
-
-        for (let i = 0; i < width; i++) {
-
-            const x = side2.x;
-            const y = side2.y;
-
-            robot.moveMouseSmooth(x, y + i);
-            robot.mouseToggle('down');
-        };
-
-        const side3 = robot.getMousePos();
-
-        for (let i = 0; i < width; i++) {
-
-            const x = side3.x;
-            const y = side3.y;
-
-            robot.moveMouseSmooth(x - i, y);
-            robot.mouseToggle('down');
-        };
-
-        const side4 = robot.getMousePos();
-
-        for (let i = 0; i < width; i++) {
-
-            const x = side4.x;
-            const y = side4.y;
-
-            robot.moveMouseSmooth(x, y - i);
-            robot.mouseToggle('down');
-            robot.mouseToggle("up");
-        };
+        robot.mouseToggle('down');
+        robot.moveMouseSmooth(x + width, y, 20);
+        robot.moveMouseSmooth(x + width, y + width, 20);
+        robot.moveMouseSmooth(x, y + width, 20);
+        robot.moveMouseSmooth(x, y, 20);
+        robot.mouseToggle('up');
 
     } catch (error) {
         console.log(error);
@@ -85,53 +50,18 @@ export const drawRectangular = async (data: RawData) => {
     try {
         const width = +data.toString().split(' ')[1];
         const length = +data.toString().split(' ')[2];
-
-        robot.setMouseDelay(5);
-
         const side = robot.getMousePos();
 
-        for (let i = 0; i < width; i++) {
-            let x = side.x;
-            let y = side.y;
+        let x = side.x;
+        let y = side.y;
 
-            robot.moveMouseSmooth(x + i, y);
-            robot.mouseToggle('down');
-        };
-
-        const side2 = robot.getMousePos();
-
-        for (let i = 0; i < length; i++) {
-
-            const x = side2.x;
-            const y = side2.y;
-
-            robot.moveMouseSmooth(x, y + i);
-            robot.mouseToggle('down');
-        };
-
-        const side3 = robot.getMousePos();
-
-        for (let i = 0; i < width; i++) {
-
-            const x = side3.x;
-            const y = side3.y;
-
-            robot.moveMouseSmooth(x - i, y, 20);
-            robot.mouseToggle('down');
-        };
-
-        const side4 = robot.getMousePos();
-
-        for (let i = 0; i < length; i++) {
-
-            const x = side4.x;
-            const y = side4.y;
-
-            robot.moveMouseSmooth(x, y - i, 20);
-            robot.mouseToggle('down');
-            robot.mouseToggle("up");
-
-        };
+        robot.mouseToggle('down');
+        robot.moveMouseSmooth(x + width, y);
+        robot.moveMouseSmooth(x + width, y + length);
+        robot.moveMouseSmooth(x + width, y + length);
+        robot.moveMouseSmooth(x, y + length);
+        robot.moveMouseSmooth(x, y)
+        robot.mouseToggle('up');
 
     } catch (error) {
         console.log(error);
