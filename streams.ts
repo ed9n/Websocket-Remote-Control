d409795d5dbd2185bb2b3f3da438433e1de7a368
Eduard Rzhautski <eduard.rzhautski@gmail.com>
2022-06-24 17:08:45 +0300

    fix: add console.log

--- streams.ts
+++ streams.ts
@@ -5,6 +5,7 @@ export const writeStream = async (ws: WebSocket, chunk: string) => {
     try {
         const duplex = createWebSocketStream(ws, { encoding: 'utf8' });
         duplex._write(chunk, 'utf-8', (err) => { });
+        console.log(`Received: ${chunk}`)
 
     } catch (error) {
         console.log(error);
