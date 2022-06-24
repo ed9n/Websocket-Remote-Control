477d545ababf5590bcb1509c3cb7c667e60d9f42
Eduard Rzhautski <eduard.rzhautski@gmail.com>
2022-06-24 17:08:16 +0300

    fix: add \0

--- mousePos.ts
+++ mousePos.ts
@@ -3,7 +3,7 @@ import robot from 'robotjs';
 export const getMousePos = () => {
 
     const mousePos = robot.getMousePos();
-    const resultMousePos = `mouse_position ${mousePos.x},${mousePos.y}`;
+    const resultMousePos = `mouse_position ${mousePos.x},${mousePos.y}\0`;
 
     return resultMousePos;
 
