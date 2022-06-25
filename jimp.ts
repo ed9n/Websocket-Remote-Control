fb7d2719e13d0c86d0805f787094fd807a49fb67
Eduard Rzhautski <eduard.rzhautski@gmail.com>
2022-06-25 18:53:43 +0300

    feat: add jimp

--- jimp.ts
+++ jimp.ts
@@ -1,31 +1,24 @@
 import robot from 'robotjs';
 import Jimp from 'jimp';
-import { writeStream } from './streams';
-
-
 
 export const jimp = async () => {
 
     const mousePos = robot.getMousePos();
-    const x = mousePos.x
-    const y = mousePos.y
+    const x = mousePos.x;
+    const y = mousePos.y;
 
-    const img = robot.screen.capture(x, y, 100, 100)
+    const screenImg = robot.screen.capture(x, y, 200, 200);
 
     const jimp = new Jimp({
-        data: img.image,
-        width: img.width,
-        height: img.height
-    })
+        data: screenImg.image,
+        width: screenImg.width,
+        height: screenImg.height
+    });
 
+    const img = await jimp.getBase64Async(Jimp.MIME_PNG);
+    const base64 = img.split(',').slice(1).toString();
 
-    const base = await jimp.getBase64Async(Jimp.MIME_PNG);
-    const base64 = base.split(',').slice(1).toString()
-    // const base64 = test.split(',').slice(1, 2).toString();
-    // const buff = test.split(',').slice(2)
+    const result = `prnt_scrn ${base64}\0`;
 
-    const result = `prnt_scrn ${base64}`
-    return result
-
-
+    return result;
 }
