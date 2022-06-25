b23920cf71f057e0347cf11b7daaa581376d867b
Eduard Rzhautski <eduard.rzhautski@gmail.com>
2022-06-25 18:55:26 +0300

    refactor: delete console.log

--- streams.ts
+++ streams.ts
@@ -5,7 +5,6 @@ export const writeStream = async (ws: WebSocket, chunk: string) => {
     try {
         const duplex = createWebSocketStream(ws, { encoding: 'utf8' });
         duplex._write(chunk, 'utf-8', (err) => { });
-        console.log(`Received: ${chunk}`)
 
     } catch (error) {
         console.log(error);
